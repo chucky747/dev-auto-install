6b393aec87547fcf82e46584f3d76dad14dfe5b1
Jbranco <james.branco@canadalife.com>
2021-08-16 18:06:37 -0400

    Added confirm messages inbetween microscripts - added standalone apache ws installer

--- vpnrunner.sh
+++ vpnrunner.sh
@@ -1,20 +1,20 @@
-# Builds Docker image
-docker build -t vpn_client_image .
-docker rm -f vpn-server
+# Builds Docker image and destroys old one
+sudo docker build -t vpn_client_image .
+sudo docker rm -f vpn-server
 
 # Runs Docker image in silent mode and in privilleged mode
-docker run -it -d --privileged --name vpn-server vpn_client_image bash
+sudo docker run -it -d --privileged --name vpn-server vpn_client_image bash
 
 # Install Dependencies inside container
-docker exec -it vpn-server apt-get -y install wget
-docker exec -it vpn-server apt update
-docker exec -it vpn-server apt -y install iproute2
+sudo docker exec -it vpn-server apt-get -y install wget
+sudo docker exec -it vpn-server apt update
+sudo docker exec -it vpn-server apt -y install iproute2
 
 # Install OVPN inside container
-docker exec -it vpn-server wget https://git.io/vpn -O openvpn-install.sh
-docker exec -it vpn-server chmod +x openvpn-install.sh
-docker exec -it vpn-server ./openvpn-install.sh
+sudo docker exec -it vpn-server wget https://git.io/vpn -O openvpn-install.sh
+sudo docker exec -it vpn-server chmod +x openvpn-install.sh
+sudo docker exec -it vpn-server ./openvpn-install.sh
 #Spit out certificate
-docker exec -it vpn-server cat /root/client.ovpn
+sudo docker exec -it vpn-server cat /root/client.ovpn
 
 
