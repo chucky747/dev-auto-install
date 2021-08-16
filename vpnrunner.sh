a01b31056d21aa48455d9634ca73e986df9adc3d
Jbranco <james.branco@canadalife.com>
2021-08-16 17:13:22 -0400

    Final vpn runner commit

--- vpnrunner.sh
+++ vpnrunner.sh
@@ -2,5 +2,21 @@
 docker build -t vpn_client_image .
 docker rm -f vpn-server
 
-# Runs Docker image - Acccess file system from inside container
-docker run -it --name vpn-server -v /Users/brajam/OneDrive\ -\ Enterprise\ 365/Cloned\ Projects/dev-auto-install:/root/vpn vpn_client_image bash
+# Runs Docker image in silent mode and in privilleged mode
+docker run -it -d --privileged --name vpn-server vpn_client_image bash
+
+
+# Send commands to container and run as container (inside)
+# Install Dependencies inside container
+docker exec -it vpn-server apt-get -y install wget
+docker exec -it vpn-server apt update
+docker exec -it vpn-server apt -y install iproute2
+
+# Install OVPN inside container
+docker exec -it vpn-server wget https://git.io/vpn -O openvpn-install.sh
+docker exec -it vpn-server chmod +x openvpn-install.sh
+docker exec -it vpn-server ./openvpn-install.sh
+#Spit out certificate
+docker exec -it vpn-server cat /root/client.ovpn
+
+
