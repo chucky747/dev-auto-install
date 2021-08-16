4ff2124a3caafe6a824008ba4b40bcd019eceb7b
jtmb <86915618+jtmb@users.noreply.github.com>
2021-08-15 22:49:10 -0400

    added jenkins print password

--- runner.sh
+++ runner.sh
@@ -5,10 +5,10 @@ mkdir jenkins_home
 # Setup Jekins Docker Image
 sudo docker pull jenkins/jenkins
 
-sudo docker run -d --name jenkins-server jenkins_image -p 8090:8080 -p 50000:50000 -v /home/testvm/jenkins_home:/var/jenkins_home jenkins/jenkins:lts-jdk11;
+sudo docker run -d --name jenkins-server -p 8090:8080 -p 50000:50000 -v /home/testvm/jenkins_home:/var/jenkins_home jenkins/jenkins:lts-jdk11;
 
 #Setup Appache Docker image
-sudo docker run -d --name apache-server apache_image -p 8091:80 -v "$PWD":/usr/local/apache2/htdocs/ httpd:2.4
+sudo docker run -d --name apache-server -p 8091:80 -v "$PWD":/usr/local/apache2/htdocs/ httpd:2.4
 
 #Forward ports for Jenkins and apache
 sudo ufw enable
@@ -19,5 +19,10 @@ sudo ufw allow from any to any port 8091 proto tcp
 docker -ps
 docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' jenkins-server
 docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' apache-server
+
+echo Printing Jenkins password
+sudo docker exec -it jenkins-server /bin/bash   
+cat /var/jenkins_home/secrets/initialAdminPassword
+exit
 
 echo Done!
